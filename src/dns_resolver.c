53340f5e81ab2ed2ac2fa63c2f6dd2ff9474ddb6
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-11-09 00:24:45 +0300

    Improve error handling & code formatting

--- src/dns_resolver.c
+++ src/dns_resolver.c
@@ -1,59 +1,65 @@
 #include <arpa/inet.h>
 #include <err.h>
-#include <netdb.h>      // For getaddrinfo function
-#include <stdio.h>      // For printf and fprintf functions
+#include <netdb.h> // For getaddrinfo function
+#include <stdio.h> // For printf and fprintf functions
+#include <stdlib.h>
 #include <string.h>     // For memset function
 #include <sys/socket.h> // For socket functions
 #include <sys/types.h>  // For data types
 #include <unistd.h>     // For close function
 
 void usage(const char *program) {
-  printf("Usage: %s hostname\n", program);
-  printf("Example: %s google.com\n", program);
+    printf("Usage: %s hostname\n", program);
+    printf("Example: %s google.com\n", program);
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    usage(argv[0]);
-    return 0;
-  }
-
-  const char *hostname = argv[1];
-  const char *servname = "http";
-
-  struct addrinfo *result;
-  int status;
-  if ((status = getaddrinfo(hostname, servname, NULL, &result) < 0)) {
-    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-    return 1;
-  };
-
-  printf("IP addresses for %s:\n\n", hostname);
-
-  struct addrinfo *p;
-  for (p = result; p != NULL; p = p->ai_next) {
-    void *addr;
-    char *ipver;
-
-    // Get the pointer to the address itself,
-    // different fields in IPv4 and IPv6:
-    if (result->ai_family == AF_INET) { // IPv4
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *)result->ai_addr;
-      addr = &(ipv4->sin_addr);
-      ipver = "IPv4";
-    } else { // IPv6
-      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)result->ai_addr;
-      addr = &(ipv6->sin6_addr);
-      ipver = "IPv6";
+    if (argc != 2) {
+        usage(argv[0]);
+        return EXIT_SUCCESS;
     }
 
-    // Convert the IP to a string and print it:
-    char ip[INET6_ADDRSTRLEN];
-    inet_ntop(result->ai_family, addr, ip, sizeof(ip));
-    printf("  %s: %s\n", ipver, ip);
-  }
+    const char *hostname = argv[1];
+    const char *servname = "http";
 
-  freeaddrinfo(result); // Free the linked list
+    struct addrinfo *result;
+    int status = getaddrinfo(hostname, servname, NULL, &result);
+    if (status != 0) {
+        fprintf(stderr, "Error resolving hostname \"%s\": %s\n", argv[1], gai_strerror(status));
+        return EXIT_FAILURE;
+    }
 
-  return 0;
+    if (result == NULL) {
+        printf("The hostname \"%s\" is syntactically valid but does not have an IP address.\n", argv[1]);
+        return EXIT_SUCCESS;
+    }
+
+    printf("IP addresses for %s:\n\n", hostname);
+
+    struct addrinfo *p;
+    for (p = result; p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        // Get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (result->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)result->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)result->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        // Convert the IP to a string and print it:
+        char ip[INET6_ADDRSTRLEN];
+        inet_ntop(result->ai_family, addr, ip, sizeof(ip));
+        printf("  %s: %s\n", ipver, ip);
+    }
+
+    freeaddrinfo(result); // Free the linked list
+
+    return 0;
 }
